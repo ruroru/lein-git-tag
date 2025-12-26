bafdfbe45a06d94cc578e90f02289ee71fd389db
ruroru <111705692+ruroru@users.noreply.github.com>
2025-12-26 14:02:15 +0000

    Stepping project version for the next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.jj/lein-git-tag "1.0.0"
+(defproject org.clojars.jj/lein-git-tag "1.0.1-SNAPSHOT"
   :description "Leiningen plugin to add version git tag"
   :url "https://github.com/ruroru/lein-git-tag"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
